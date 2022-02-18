5759d4abbe17cb0b8e352b9189f41f635feb7752
WalleksMR <walleksmiranda@gmail.com>
2022-02-18 19:52:54 -0300

    add component player

---
+++ src/pages/player/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import BannerPlay from "../../components/BannerPlay";
+
+const Player: NextPage = () => {
+  return <BannerPlay />;
+};
+
+export default Player;
